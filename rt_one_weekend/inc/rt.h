099bd5e673c213c9a79bc1ca7e612ade435a8535
spenning <whimizera@mailbox.org>
2024-10-11 16:21:06 +0200

    added utils

---
+++ rt_one_weekend/inc/rt.h
@@ -0,0 +1,10 @@
+#ifndef RT_H
+#define RT_H
+
+#define pi 3.1415926535897932385
+
+double degrees_to_radians(double degrees);
+
+#endif
+
+
---
+++ rt_one_weekend/src/utils.c
@@ -0,0 +1,6 @@
+#include <rt.h>
+
+double degrees_to_radians(double degrees)
+{
+	return (degrees * pi / 180.0);
+}
