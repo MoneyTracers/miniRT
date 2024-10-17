09fa121d95c42731df76973d6337885056ec9dcb
spenning <whimizera@mailbox.org>
2024-10-17 15:51:17 +0200

    gamma corrected image

--- rt_one_weekend/src/color.c
+++ rt_one_weekend/src/color.c
@@ -1,5 +1,12 @@
 #include <rt.h>
 
+double linear_to_gamma(double linear_component)
+{
+	if (linear_component > 0)
+		return (sqrt(linear_component));
+	return (0);
+}
+
 void write_color(t_vec3 vec3)
 {
 	int rbyte;
@@ -7,8 +14,8 @@ void write_color(t_vec3 vec3)
 	int bbyte;
 	static t_interval intensity;
 	intensity = inv(0.000, 0.999);
-	rbyte = (int)256*(clamp(intensity, vec3.e[0]));
-	gbyte = (int)256*(clamp(intensity, vec3.e[1]));
-	bbyte = (int)256*(clamp(intensity, vec3.e[2]));
+	rbyte = (int)256*(clamp(intensity, linear_to_gamma(vec3.e[0])));
+	gbyte = (int)256*(clamp(intensity, linear_to_gamma(vec3.e[1])));
+	bbyte = (int)256*(clamp(intensity, linear_to_gamma(vec3.e[2])));
 	printf("%d %d %d\n", rbyte, gbyte, bbyte);
 }
