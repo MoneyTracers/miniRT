52887df509e4f90235c8edf460f864b96033460c
spenning <whimizera@mailbox.org>
2024-10-22 16:02:17 +0200

    hollow glass sphere

--- rt_one_weekend/src/main.c
+++ rt_one_weekend/src/main.c
@@ -4,7 +4,6 @@ int main ()
 {
 	t_hittable *world;
 	t_camera	cam;
-	t_hittable *temp;
 
 	// calculate image height
 	cam.aspect_ratio = 16.0 / 9.0;
@@ -12,10 +11,10 @@ int main ()
 
 	//world 
 	world = lstnew(sphere, vec(1, 0, -1), 0.5, init_mat(metal, vec(0.8, 0.6, 0.2), 0.3, 0));
-	lstadd(&world, lstnew(sphere, vec(-1.0, -0, -1), 0.5, init_mat(dielectric, vec(0, 0, 0), 0, 1.00/1.33)));
+	lstadd(&world, lstnew(sphere, vec(-1.0, -0, -1), 0.5, init_mat(dielectric, vec(0, 0, 0), 0, 1.5)));
+	lstadd(&world, lstnew(sphere, vec(-1.0, -0, -1), 0.4, init_mat(dielectric, vec(0, 0, 0), 0, 1.00/1.5)));
 	lstadd(&world, lstnew(sphere, vec(0, 0, -1.2), 0.5, init_mat(lambertian, vec(0.1, 0.2, 0.5), 0, 0)));
 	lstadd(&world, lstnew(sphere, vec(0, -100.5, -1), 100, init_mat(lambertian, vec(0.8, 0.8, 0.0), 0, 0)));
-	temp = world;
 	render(&cam, world);
 	return (0);
 }
