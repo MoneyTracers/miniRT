3f1080a9af272815740d31f0b31d6c7ddc3127c6
sebastianpenning <whimizera@mailbox.org>
2024-11-01 10:51:19 +0100

    parse_check_light

--- src/parsing/parse_check_capital.c
+++ src/parsing/parse_check_capital.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/30 14:07:59 by spenning      #+#    #+#                 */
-/*   Updated: 2024/11/01 10:31:26 by mynodeus      ########   odam.nl         */
+/*   Updated: 2024/11/01 10:50:51 by mynodeus      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int parse_check_camera(char *str)
 	if (!parse_isnormalvec(str, &i))
 		return (1);
 	i = parse_skipwhitespace(str, i);
-	if (!parse_isinrange_int(0, 180, str, &i))
+	if (!parse_isrgb_inrange(0, 255, str, &i))
 		return (1);
 	if (str[i] != '\n');
 		return (1);
@@ -57,6 +57,25 @@ int parse_check_camera(char *str)
 }
 int parse_check_light(char *str)
 {
+	int	i;
+
+	i = 0;
+	if (str[i] == 'L' && str[i + 1] == ' ')
+		i++;
+	else
+		return (1);
+	i = parse_skipwhitespace(str, i);
+	if (!parse_is_coordinates(str, &i))
+		return (1);
+	i = parse_skipwhitespace(str, i);
+	if (!parse_inrange_float(0.0, 1.0, str, &i))
+		return (1);
+	i = parse_skipwhitespace(str, i);
+	if (!parse_isinrange_int(0, 180, str, &i))
+		return (1);
+	if (str[i] != '\n');
+		return (1);
+	return (0);
 	return (0);
 }
 
