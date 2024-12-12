3e29866891617d0b987817f4dab172d25d5e06ee
spenning <whimizera@mailbox.org>
2024-12-12 14:56:06 +0100

    removed hooks

--- src/mini_mlx/hooks.c
+++ src/mini_mlx/hooks.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   hooks.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maraasve <maraasve@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/22 15:49:59 by maraasve          #+#    #+#             */
-/*   Updated: 2024/12/03 17:34:59 by maraasve         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   hooks.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: maraasve <maraasve@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/11/22 15:49:59 by maraasve      #+#    #+#                 */
+/*   Updated: 2024/12/12 14:55:16 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,6 @@
 #include <matrix.h>
 #include <transformation.h>
 
-int	move_cam(int keycode, t_camera *cam)
-{
-	t_tuple		from;
-	t_tuple		to;
-
-	from = cam->pos;
-	if (keycode == UP_KEY)
-		from = add_tuple(from, scale_tuple(cam->true_up, 10));
-	else if (keycode == DOWN_KEY)
-		from = subtract_tuple(from, scale_tuple(cam->true_up, 10));
-	else if (keycode == LEFT_KEY)
-		from = add_tuple(from, scale_tuple(cam->left, 10));
-	else if (keycode == RIGHT_KEY)
-		from = subtract_tuple(from, scale_tuple(cam->left, 10));
-	else if (keycode == ZOOM_IN)
-		from = add_tuple(from, scale_tuple(cam->forward, 10));
-	else if (keycode == ZOOM_OUT)
-		from = subtract_tuple(from, scale_tuple(cam->forward, 10));
-	to = add_tuple(from, cam->forward);
-	cam->transformation = view_transform(cam, from, to, create_vector(0, 1, 0));
-	cam->inverse = invert_matrix(cam->transformation, 4);
-	return (1);
-}
-
 int	keypress(int keycode, t_mlx *data)
 {
 	t_camera	*cam;
@@ -50,18 +26,6 @@ int	keypress(int keycode, t_mlx *data)
 	{
 		mlx_loop_end(data->mlx);
 		return (1);
-	}
-	if (keycode == UP_KEY || keycode == DOWN_KEY || keycode == LEFT_KEY || \
-		keycode == RIGHT_KEY || keycode == ZOOM_IN || keycode == ZOOM_OUT)
-	{
-		move_cam(keycode, cam);
-		render(data, *cam, data->world);
-		if (data->img_count % 2)
-			mlx_put_image_to_window(data->mlx, data->window, \
-									data->img1.image, 0, 0);
-		else
-			mlx_put_image_to_window(data->mlx, data->window, \
-									data->img2.image, 0, 0);
 	}
 	return (1);
 }
